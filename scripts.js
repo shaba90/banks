17a644c372a50c14792f4b2c09862b6288df67fc
shaba90 <shabaphilip6@gmail.com>
2023-07-09 13:13:30 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevent form submission
+  
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+  
+    // Perform verification
+    if (username === "Bancosantander@gmail.com" && password === "Bancosan007") {
+      // Redirect to another page
+      window.location.href = "nextt.html";
+    } else {
+      document.getElementById("error").textContent = "Invalid username or password";
+    }
+     // Show logout container and start logout timer
+     loginForm.reset();
+     loginForm.style.display = 'none';
+     logoutContainer.style.display = 'block';
+ 
+     // Logout after 1 minute
+     setTimeout(logout, 60000);
+  });
